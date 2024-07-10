8355e1e2c1b84b4ed83a984ee2c00bd469edb217
Ibrahim H. <bitsnaps@yahoo.fr>
2024-07-10 11:26:53 +0000

    move to replit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
@@ -9,14 +8,20 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*"
-        ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def root():
     return {"message": "hello"}
+
+
+@app.post("/chat")
+async def chat_completion(request: Request):
+    data = await request.json()
+    print(f"****** pass POST: {data}")
+    return data  #['message']
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <script setup lang="ts">
-// You don't need to import these manually
-// import { ref } from 'vue'
-// import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue'
+import { formatDate } from '@vueuse/core'
 const message = ref('')
 
-const showMsg = async () => {
-    const response = await fetch('http://127.0.0.1:8000/', { 
-      credentials: 'include' // for google's IDX only.
+
+const postRequest = async () => {
+
+  try {
+    // fetch request using POST
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/chat`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "message": 'Tell me a joke.'
+      })
     })
-    const data = await response.json()
-    message.value = data.message
+
+    if (response.ok) {
+      const data = await response.json()
+      if (data) {
+        message.value = `${data.message} at: ${formatDate(new Date(), 'HH:mm:ss')}`
+      } else {
+        message.value = 'error'
+      }
+    }
+  } catch (e) {
+    message.value = `${e} at: ${formatDate(new Date(), 'HH:mm:ss')}`
+  }
+}
+
+const showMsg = async () => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}`)
+  const data = await response.json()
+  message.value = data.message
 }
 
 </script>
 
 <template>
   <div>
-      
-      <button @click="showMsg">Show message</button>
-      <h1 v-if="message">{{ $t(`message.${message}`) }}</h1>
 
-      <nav>
-      <RouterLink to="/">Home</RouterLink> | 
+    <BButton variant="primary" @click="postRequest">Chat</BButton>
+
+    <BButton variant="success" @click="showMsg">Show message</BButton>
+
+    <h1 v-if="message">{{ message }}</h1>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
       <RouterLink to="/about">About</RouterLink>
     </nav>
     <HelloWorld :msg="message" />
@@ -36,9 +63,11 @@ const showMsg = async () => {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
